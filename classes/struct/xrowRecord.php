967c05b78065b317d95b07dcc021f453fa67a254
xinyuexrow <wang@xrow.de>
2013-06-18 16:01:57 +0200

    add further fields for schluetersche search

--- classes/struct/xrowRecord.php
+++ classes/struct/xrowRecord.php
@@ -6,7 +6,8 @@ class xrowRecord implements ezcBasePersistable, ezcSearchDefinitionProvider
     public $meta_guid;
     public $meta_name;
     public $meta_main_url_alias;
-    public $text;
+    public $meta_description_text;
+    public $meta_image_url;
     public $meta_installation_id;
     public $meta_language_code;
     public $meta_anon_access;
@@ -14,7 +15,7 @@ class xrowRecord implements ezcBasePersistable, ezcSearchDefinitionProvider
     public $meta_class_identifier;
     public $meta_section_id;
     const VIRTUAL_CLASSIDENTIFIER = "external";
-    function __construct( $installation_id = false, $language = 'ger-DE', $meta_guid = null, $meta_name = null, $meta_main_url_alias = null, $text = null, $keywords = null )
+    function __construct( $installation_id = false, $language = 'ger-DE', $meta_guid = null, $meta_name = null, $meta_main_url_alias = null, $meta_description_text = null,$meta_image_url=null, $keywords = null )
     {
         if ( $meta_guid === null )
         {
@@ -26,8 +27,9 @@ class xrowRecord implements ezcBasePersistable, ezcSearchDefinitionProvider
         }
         $this->meta_name = $meta_name;
         $this->meta_main_url_alias = $meta_main_url_alias;
+        $this->meta_image_url = $meta_image_url;
         $this->meta_key = $keywords;
-        $this->text = $text;
+        $this->meta_description_text = $meta_description_text;
         $this->meta_is_invisible = false;
         $this->meta_section_id = 1;
         $this->meta_language_code = $language;
@@ -59,14 +61,14 @@ class xrowRecord implements ezcBasePersistable, ezcSearchDefinitionProvider
         $n->fields['meta_key'] = new ezcSearchDefinitionDocumentField( 'meta_key', xrowSOLRHandler::KEY );
         $n->fields['meta_name'] = new ezcSearchDefinitionDocumentField( 'meta_name', ezcSearchDocumentDefinition::TEXT, 2, true, false, true );
         $n->fields['meta_main_url_alias'] = new ezcSearchDefinitionDocumentField( 'meta_main_url_alias', xrowSOLRHandler::MS );
-        $n->fields['text'] = new ezcSearchDefinitionDocumentField( 'text', ezcSearchDocumentDefinition::TEXT, 1, false, false, true );
+        $n->fields['meta_image_url'] = new ezcSearchDefinitionDocumentField( 'meta_image_url', xrowSOLRHandler::MS );
+        $n->fields['meta_description_text'] = new ezcSearchDefinitionDocumentField( 'meta_description_text', ezcSearchDocumentDefinition::TEXT, 1, false, false, true );
         return $n;
     }
 
     function getState()
     {
         $state = array( 
-        
             'meta_installation_id' => $this->meta_installation_id ,
             'meta_section_id' => $this->meta_section_id ,
             'meta_language_code' => $this->meta_language_code , 
@@ -76,8 +78,9 @@ class xrowRecord implements ezcBasePersistable, ezcSearchDefinitionProvider
             'meta_anon_access' => $this->meta_anon_access ,
             'meta_key' => $this->meta_key , 
             'meta_name' => $this->meta_name , 
-            'meta_main_url_alias' => $this->meta_main_url_alias , 
-            'text' => $this->text 
+            'meta_main_url_alias' => $this->meta_main_url_alias ,
+            'meta_image_url' => $this->meta_image_url ,
+            'meta_description_text' => $this->meta_description_text 
         );
         return $state;
     }
