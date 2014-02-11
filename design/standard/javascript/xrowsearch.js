833cecb81fd80a00045ea82cea8a1bb09120a323
Kristina <kristina@xrow.de>
2014-02-11 13:27:09 +0100

    optimization

--- design/standard/javascript/xrowsearch.js
+++ design/standard/javascript/xrowsearch.js
@@ -1,18 +1,25 @@
-function initOverlayBlocker(divID, callbackFunc) {
-    jQuery('body').append(jQuery('<div/>', {
-        id: divID
-    }));
-    jQuery('#'+id).css('display', 'block').click(function() {
-        jQuery(this).css('display', 'none');
-        if(typeof callback != 'undefined')
-            eval(callbackFunc+'();');
-        
-    });
-};
-function loadAutocomplete(element) {
-    var position = element.position();
-    var contentparent = jQuery('#'+divautocomplete+'-parent-content'),
-        htmlcontent = contentparent.html(),
-        content = '<div id="'+divautocomplete+'-content">'+htmlcontent+'</div>';
-    jQuery('#'+divautocomplete).html(content).css({ display: "block", height: 0 }).animate({ height: height+'px' }, 600);
-};
+jQuery.extend({
+    initOverlayBlocker : function(divID, callbackFunc){
+        jQuery('body').append(jQuery('<div/>', {
+            id: divID
+        }));
+        jQuery('#'+divID).css('display', 'block').click(function() {
+            jQuery(this).css('display', 'none');
+            if(typeof callbackFunc == 'function'){
+                callbackFunc.call(this);
+            }
+        });
+    }
+});
+jQuery.extend({
+    loadAutocomplete : function(element, divautocomplete, height, callbackFunc){
+        var position = element.position();
+        var contentparent = jQuery('#'+divautocomplete+'-parent-content'),
+            htmlcontent = contentparent.html(),
+            content = '<div id="'+divautocomplete+'-content">'+htmlcontent+'</div>';
+        jQuery('#'+divautocomplete).html(content).css({ display: "block", height: 0 }).animate({ height: height+'px' }, 600);
+        if(typeof callbackFunc == 'function'){
+            callbackFunc.call(this);
+        }
+    }
+});
